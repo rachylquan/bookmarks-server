79ae794af271895fe0c46a71c887c920f88029e8
Rachyl <rachylquan@yahoo.com>
2020-08-13 15:31:32 -0700

    url validation

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { v4: uuid } = require('uuid')
 const logger = require('../logger')
 const { bookmarks } = require('../store')
+const { isWebUri } = require('valid-url')
 
 const bookmarksRouter = express.Router()
 const bodyParser = express.json()
@@ -21,6 +22,11 @@ bookmarksRouter
       }
     }
 
+    if (!isWebUri(url)) {
+      logger.error(`Invalid url: '${url}'`)
+      return res.status(400).send(`'url' must be a valid URL`)
+    }
+
     if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
       logger.error(`Invalid rating '${rating}' supplied`)
       return res.status(400).send(`'rating' must be a number between 0 and 5`)
@@ -30,7 +36,7 @@ bookmarksRouter
 
     bookmarks.push(bookmark)
 
-    logger.info(`Bookmark with id ${bookmark.id} created`)
+    logger.info(`Bookmark with id: ${bookmark.id} created`)
     res
       .status(201)
       .location(`http://localhost:8000/bookmarks/${bookmark.id}`)
@@ -45,7 +51,7 @@ bookmarksRouter
 
     // make sure we found a bookmark
     if (!bookmark) {
-      logger.error(`Bookmark with id ${id} not found.`);
+      logger.error(`Bookmark with id: ${id} not found.`);
       return res
         .status(404)
         .send('Bookmark Not Found');
@@ -59,7 +65,7 @@ bookmarksRouter
     const bookmarkIndex = bookmarks.findIndex(li => li.id == id);
 
     if (bookmarkIndex === -1) {
-      logger.error(`Bookmark with id ${id} not found.`);
+      logger.error(`Bookmark with id: ${id} not found.`);
       return res
         .status(404)
         .send('Not Found');
@@ -67,7 +73,7 @@ bookmarksRouter
 
     bookmarks.splice(bookmarkIndex, 1);
 
-    logger.info(`Bookmark with id ${id} deleted.`);
+    logger.info(`Bookmark with id: ${id} deleted.`);
     res
       .status(204)
       .end();
