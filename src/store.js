188c5296dcc1658b370b00c405c0212a72c24827
Rachyl <rachylquan@yahoo.com>
2020-08-13 15:19:44 -0700

    routes, get, post, delete

---
+++ src/store.js
@@ -0,0 +1,14 @@
+const bookmarks = [
+  { id: 1,
+    title: 'Thinkful',
+    url: 'https://www.thinkful.com',
+    description: 'Think outside the classroom',
+    rating: 5 },
+  { id: 2,
+    title: 'Google',
+    url: 'https://www.google.com',
+    description: 'Where we find everything else',
+    rating: 4 },
+]
+
+module.exports = { bookmarks }
